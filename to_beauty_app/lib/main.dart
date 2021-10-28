b37d77bbd538f6fca89ee8aed06e2f5c06c2d76c
Hilton Santos <jhilton930@gmail.com>
2021-10-28 17:51:46 -0300

    Update main

--- to_beauty_app/lib/main.dart
+++ to_beauty_app/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:to_beauty_app/src/pages/splash/splash_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(const SplashPage());
 }
